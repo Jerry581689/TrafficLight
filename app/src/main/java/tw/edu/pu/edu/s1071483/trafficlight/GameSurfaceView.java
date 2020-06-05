b1d4431216a64178296ad952b6b90316254df0f8
jerry <A>
2020-06-05 13:51:46 +0800

    2020上機考版本1(莊育晟)

--- app/src/main/java/tw/edu/pu/edu/s1071483/trafficlight/GameSurfaceView.java
+++ app/src/main/java/tw/edu/pu/edu/s1071483/trafficlight/GameSurfaceView.java
@@ -11,12 +11,13 @@ import android.util.AttributeSet;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-public class GameSurfaceView extends SurfaceView implements SurfaceHolder.Callback{
+public class GameSurfaceView extends SurfaceView implements SurfaceHolder.Callback {
 
     SurfaceHolder surfaceHolder;
     Bitmap Road, Boy;
 
     int GreenLightSec, YellowLightSec, RedLightSec; //各燈號秒數
+    int GreenLightSec_cnt, YellowLightSec_cnt, RedLightSec_cnt; //各燈號秒數
     Boolean BoyMoving = false; //小男孩是否移動
     int BGmoveX = 0; //背景圖片往左捲動像素
 
@@ -25,6 +26,8 @@ public class GameSurfaceView extends SurfaceView implements SurfaceHolder.Callba
     float ratio, w, h; //比例及寬度與長度
 
     int step = 1; //步數
+    int picture = 1;
+    int nowLight;
 
 
     public GameSurfaceView(Context context, AttributeSet attrs) {
@@ -52,7 +55,7 @@ public class GameSurfaceView extends SurfaceView implements SurfaceHolder.Callba
     }
 
     //道路背景圖繪製
-    public void DrawRoad(Canvas canvas){
+    public void DrawRoad(Canvas canvas) {
         //調整圖片高度為裝置解析度高度的95%
         SrcRect = new Rect(0, 0, Road.getWidth(), Road.getHeight());
         ratio = Road.getHeight() / (canvas.getHeight() * 0.95f);
@@ -60,34 +63,35 @@ public class GameSurfaceView extends SurfaceView implements SurfaceHolder.Callba
         h = Road.getHeight() / ratio;
 
         //處理背景捲動
-        if (BoyMoving){
+        if (BoyMoving) {
             BGmoveX -= 5;
         }
-        int BGnewX = (int)w + BGmoveX;
+        int BGnewX = (int) w + BGmoveX;
 
         // 如果已捲動整張圖則重新開始，否則用兩張圖拼裝
         if (BGnewX <= 0) {
             BGmoveX = 0;
             // only need one draw
             DestRect = new Rect(0, 0, (int) w, (int) h);
-            canvas.drawBitmap(Road , SrcRect, DestRect, null);
+            canvas.drawBitmap(Road, SrcRect, DestRect, null);
         } else {
             // need to draw original and wrap
-            DestRect = new Rect(BGmoveX, 0, (int) (w+BGmoveX), (int) h);
-            canvas.drawBitmap(Road , SrcRect, DestRect, null);
-            DestRect = new Rect(BGnewX, 0, (int) (w+BGnewX), (int) h);
-            canvas.drawBitmap(Road , SrcRect, DestRect, null);
+            DestRect = new Rect(BGmoveX, 0, (int) (w + BGmoveX), (int) h);
+            canvas.drawBitmap(Road, SrcRect, DestRect, null);
+            DestRect = new Rect(BGnewX, 0, (int) (w + BGnewX), (int) h);
+            canvas.drawBitmap(Road, SrcRect, DestRect, null);
         }
     }
 
     //小男孩繪製
-    public void DrawBoy(Canvas canvas){
-        if (BoyMoving){  //分數加1，並改變小男孩走路圖示
+    public void DrawBoy(Canvas canvas) {
+        if (BoyMoving) {  //分數加1，並改變小男孩走路圖示
+            picture++;
             step++;
-            if (step>8){
-                step = 1;
+            if (picture > 8) {
+                picture = 1;
             }
-            int res = getResources().getIdentifier("boy" + (step), "drawable", getContext().getPackageName());
+            int res = getResources().getIdentifier("boy" + (picture), "drawable", getContext().getPackageName());
             Boy = BitmapFactory.decodeResource(getResources(), res);
         }
 
@@ -95,17 +99,17 @@ public class GameSurfaceView extends SurfaceView implements SurfaceHolder.Callba
         SrcRect = new Rect(0, 0, Boy.getWidth(), Boy.getHeight());
         float w = Boy.getWidth() / ratio;
         float h = Boy.getHeight() / ratio;
-        float w0 = canvas.getHeight()* 0.2f;
-        float h0 = canvas.getHeight()* 0.93f - h;
-        DestRect = new Rect((int) w0, (int) h0, (int) (w0+w), (int) (h0+h));
-        canvas.drawBitmap(Boy , SrcRect, DestRect, null);
+        float w0 = canvas.getHeight() * 0.2f;
+        float h0 = canvas.getHeight() * 0.93f - h;
+        DestRect = new Rect((int) w0, (int) h0, (int) (w0 + w), (int) (h0 + h));
+        canvas.drawBitmap(Boy, SrcRect, DestRect, null);
 
         //目前步數繪製
         paint.setColor(Color.BLUE);
         paint.setStyle(Paint.Style.FILL);
         paint.setTextSize((int) 60 * canvas.getHeight() / 1080);
         paint.setAntiAlias(true);
-        canvas.drawText("圖片編號：" + String.valueOf(step), (int) (0), (int) (canvas.getHeight()*0.1) ,paint);
+        canvas.drawText("步數：" + String.valueOf(step), (int) (0), (int) (canvas.getHeight() * 0.1), paint);
     }
 
     //紅綠燈繪製
@@ -113,8 +117,8 @@ public class GameSurfaceView extends SurfaceView implements SurfaceHolder.Callba
         //長方形區域黑色背景
         paint.setColor(Color.BLACK);
         int r = (int) 100 * canvas.getHeight() / 1080;
-        canvas.drawRect(canvas.getWidth()-2*r-16, 0,
-                canvas.getWidth(), 6*r+30, paint);
+        canvas.drawRect(canvas.getWidth() - 2 * r - 16, 0,
+                canvas.getWidth(), 6 * r + 30, paint);
 
         //空心圓形
         paint.setStyle(Paint.Style.STROKE);
@@ -122,31 +126,53 @@ public class GameSurfaceView extends SurfaceView implements SurfaceHolder.Callba
 
         //紅綠燈三個圓
         paint.setColor(Color.GREEN);
-        canvas.drawCircle(canvas.getWidth() - r -8, 5*r+20, r, paint);
+        canvas.drawCircle(canvas.getWidth() - r - 8, 5 * r + 20, r, paint);
         paint.setColor(Color.YELLOW);
-        canvas.drawCircle(canvas.getWidth() - r -8, 3*r+10, r, paint);
+        canvas.drawCircle(canvas.getWidth() - r - 8, 3 * r + 10, r, paint);
         paint.setColor(Color.RED);
-        canvas.drawCircle(canvas.getWidth() - r -8, r, r, paint);
+        canvas.drawCircle(canvas.getWidth() - r - 8, r, r, paint);
 
 
         //以綠燈為例，畫出實心圓
         paint.setStyle(Paint.Style.FILL_AND_STROKE);
-        paint.setColor(Color.GREEN);
-        canvas.drawCircle(canvas.getWidth() - r -8, 5*r+20, r, paint);
+        if (nowLight == 1)
+            paint.setColor(Color.GREEN);
+        else
+            paint.setColor(Color.BLACK);
+        canvas.drawCircle(canvas.getWidth() - r - 8, 5 * r + 20, r, paint);
+
+        paint.setStyle(Paint.Style.FILL_AND_STROKE);
+        if (nowLight == 2)
+            paint.setColor(Color.YELLOW);
+        else
+            paint.setColor(Color.BLACK);
+        canvas.drawCircle(canvas.getWidth() - r - 8, 3 * r + 10, r, paint);
+
+        paint.setStyle(Paint.Style.FILL_AND_STROKE);
+        if (nowLight == 3)
+            paint.setColor(Color.RED);
+        else
+            paint.setColor(Color.BLACK);
+        canvas.drawCircle(canvas.getWidth() - r - 8, r, r, paint);
 
         //顯示各燈號秒數
         paint.setColor(Color.BLUE);
         paint.setTextSize(r);
-        canvas.drawText(String.valueOf(GreenLightSec), canvas.getWidth() - 1.5f *r, 5.5f * r + 10 , paint);
-        canvas.drawText(String.valueOf(YellowLightSec), canvas.getWidth() - 1.5f *r, 3.5f * r + 5 , paint);
-        canvas.drawText(String.valueOf(RedLightSec), canvas.getWidth() - 1.5f *r, 1.5f * r  , paint);
+        canvas.drawText(String.valueOf(GreenLightSec_cnt), canvas.getWidth() - 1.5f * r, 5.5f * r + 10, paint);
+        canvas.drawText(String.valueOf(YellowLightSec_cnt), canvas.getWidth() - 1.5f * r, 3.5f * r + 5, paint);
+        canvas.drawText(String.valueOf(RedLightSec_cnt), canvas.getWidth() - 1.5f * r, 1.5f * r, paint);
     }
 
     //初始設定各燈號秒數
-    public void SetLightSec(int GreenSec, int YellowSec, int RedSec){
+    public void SetLightSec(int GreenSec, int YellowSec, int RedSec) {
         GreenLightSec = GreenSec;
         YellowLightSec = YellowSec;
         RedLightSec = RedSec;
+        nowLight = 1;
+
+        GreenLightSec_cnt = GreenSec;
+        YellowLightSec_cnt = YellowSec;
+        RedLightSec_cnt = RedSec;
     }
 
 
@@ -158,6 +184,26 @@ public class GameSurfaceView extends SurfaceView implements SurfaceHolder.Callba
     @Override
     public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
 
+    }
+
+    public void updateLight(Canvas canvas) {
+        if (GreenLightSec_cnt > 0) {
+            GreenLightSec_cnt--;
+            nowLight = 1;
+        } else if (YellowLightSec_cnt > 0) {
+            YellowLightSec_cnt--;
+            nowLight = 2;
+        } else if (RedLightSec_cnt > 0) {
+            RedLightSec_cnt--;
+            nowLight = 3;
+        } else {
+            GreenLightSec_cnt = GreenLightSec;
+            YellowLightSec_cnt = YellowLightSec;
+            RedLightSec_cnt = RedLightSec;
+            nowLight = 1;
+        }
+
+        drawSomething(canvas);
     }
 
 }
--- app/src/main/java/tw/edu/pu/edu/s1071483/trafficlight/MainActivity.java
+++ app/src/main/java/tw/edu/pu/edu/s1071483/trafficlight/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
         //設定螢幕為橫式
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
 
 
         setContentView(R.layout.activity_main);
